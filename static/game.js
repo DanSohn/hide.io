e6ba16cc5954885db0a4a89fb64916aa7413b371
Daniel <chiefgustavo000@gmail.com>
2020-02-27 18:49:53 -0700

    Added basic keyboard arrow event handlers

---
+++ static/game.js
@@ -0,0 +1,45 @@
+let socket = io();
+
+socket.on("message", (data) =>{
+   console.log(data)
+});
+
+let movement = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+};
+
+document.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 87: // W
+            movement.up = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+        case 83: // S
+            movement.down = true;
+            break;
+    }
+});
+document.addEventListener('keyup', function(event) {
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 87: // W
+            movement.up = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+        case 83: // S
+            movement.down = false;
+            break;
+    }
+});
