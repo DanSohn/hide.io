4d4cf688b1af5a29cfd355e4e6486909c45b0e09
Daniel <chiefgustavo000@gmail.com>
2020-02-27 22:52:24 -0700

    Created basic react app

---
+++ src/game/Game.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Game extends Component{
+    render(){
+        return (
+            <div>
+                <h1> Testing ...</h1>
+            </div>
+        )
+    }
+}
+
+export default Game
---
+++ temp/game/Game.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Game extends Component{
+    render(){
+        return (
+            <div>
+                <h1> Testing ...</h1>
+            </div>
+        )
+    }
+}
