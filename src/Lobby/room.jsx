192f232381c1f9e78576518472e31e54d1503808
peternamkoong <peternamkoong@gmail.com>
2020-03-24 14:47:52 -0600

    connected the room screen to the game screen

--- src/Lobby/room.jsx
+++ src/Lobby/room.jsx
@@ -8,6 +8,7 @@ import { socket } from "../assets/socket";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "../assets/App.css";
 import ViewLobbies from "./viewLobbies";
+import Game from "../Game/Game";
 
 class Room extends Component {
     constructor(props) {
@@ -17,9 +18,13 @@ class Room extends Component {
             email: this.props.email,
             image: this.props.image,
             title: "Room Name should be from database?",
-            start: false
+            start: false,
+            numPlayers: 0,
+            players: {}
         };
         this.goPrevious = this.goPrevious.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.start = this.start.bind(this);
     }
 
     goPrevious() {
@@ -27,7 +32,43 @@ class Room extends Component {
             previous: true
         });
     }
-    start() {}
+    startTimer() {
+        // 3 second timer currently
+        socket.emit("lobby start timer", 3100);
+    }
+    start() {
+        this.setState({
+            start: true
+        });
+    }
+
+    componentDidMount() {
+        console.log("finished rendering");
+        socket.emit("player joined");
+        socket.on("Number of players", num_players => {
+            console.log("number of players ", num_players);
+            this.setState({
+                numPlayers: num_players
+            });
+        });
+
+        socket.on("players list", players => {
+            console.log("Recieved list of players");
+            console.log(players);
+            this.setState({
+                players: players
+            });
+        });
+
+        socket.on("lobby current timer", countdown => {
+            console.log(countdown);
+            // after i reach 0, call startGame
+            if (countdown <= 0) {
+                console.log("starting game");
+                this.start();
+            }
+        });
+    }
 
     render() {
         let comp;
@@ -40,6 +81,12 @@ class Room extends Component {
                 />
             );
         } else if (this.state.start) {
+            comp = (
+                <Game
+                    numPlayers={this.state.numPlayers}
+                    players={this.state.players}
+                />
+            );
         } else {
             comp = (
                 <div className="GameWindow">
@@ -61,7 +108,9 @@ class Room extends Component {
                         </div>
 
                         <div className="roomActions">
-                            <button className="btn btn-success">
+                            <button
+                                className="btn btn-success"
+                                onClick={this.startTimer}>
                                 Start Game
                             </button>
                             <h3>Game Mode:</h3>
