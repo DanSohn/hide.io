ddacba20dee29063b0ee3eff69be045d94df5828
PARSA.HONARMAND <parsa.honarmand@gmail.com>
2020-03-30 17:10:21 -0600

    socket

--- src/assets/socket.js
+++ src/assets/socket.js
@@ -1,4 +1,4 @@
 const io = require('socket.io-client');
 
-export const socket = io.connect('https://hideio.herokuapp.com');
-// export const socket = io(process.env.PORT||'localhost:3001')
+// export const socket = io.connect('https://hideio.herokuapp.com');
+export const socket = io(process.env.PORT||'localhost:3001')
