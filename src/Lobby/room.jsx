22b8998ec0a7301c6e33fe39c8a91ab5c3014557
Daniel <chiefgustavo000@gmail.com>
2020-04-07 10:42:30 -0600

    Fixed typo, where time should've been called timer, for the event lobby start timer

--- src/Lobby/room.jsx
+++ src/Lobby/room.jsx
@@ -99,7 +99,7 @@ class Room extends Component {
         // this event occurs on function startTimer()
         socket.on("game starting ack", (gameMap) => {
             this.state.game_map = gameMap;
-            socket.emit("lobby start timer", ({time: 3100, room: this.state.roomID}));
+            socket.emit("lobby start timer", ({timer: 3100, room: this.state.roomID}));
         });
 
 
