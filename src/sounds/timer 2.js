849bbde0a6cde54138bd2fcc7e9569f00d22c1e6
Daniel <chiefgustavo000@gmail.com>
2020-04-10 22:32:23 -0600

    Got rid of additional files labelled 2

--- src/sounds/timer 2.js
+++
@@ -1,9 +0,0 @@
-
-function timerSound() {
-    let soundButton = new Audio("https://freesound.org/people/fordps3/sounds/186669/download/186669__fordps3__computer-boop.wav")
-    return (
-        soundButton.play()
-    )
-}
-
-export default timerSound
