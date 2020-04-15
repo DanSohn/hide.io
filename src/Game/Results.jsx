56d6457071aa011ba39bd52b8838167c7032fd39
PARSA.HONARMAND <parsa.honarmand@gmail.com>
2020-04-15 00:03:28 -0600

    new css for result

--- src/Game/Results.jsx
+++ src/Game/Results.jsx
@@ -29,32 +29,28 @@ class Results extends Component {
         if (this.state.playerRole === "seeker" && this.state.winner === "seeker") {
             comp =
                 (<>
-                    <h1>Congrats, you've caught 'em all!</h1>
-                    <h5>returning to lobby now...</h5>
+                    <span className="btn-winner ff-20" >Congrats, you've caught 'em all!<br />returning to lobby now...</span>
                 </>)
         }
         // If client is a seeker and he loses
         else if (this.state.playerRole === "seeker" && this.state.winner === "hiders") {
             comp =
                 (<>
-                    <h5>We'll get 'em next Time</h5>
-                    <h5>returning to lobby now...</h5>
+                    <span className="btn-loser ff-20" >We'll get 'em next time!<br />returning to lobby now...</span>
                 </>)
         }
         // If client is a hider and he won
         else if (this.state.playerRole === "hider" && this.state.winner === "hiders") {
             comp =
                 (<>
-                    <h5>You and your fellow hiders have won!</h5>
-                    <h5>returning to lobby now...</h5>
+                    <span className="btn-winner ff-20" >Congrats, hiders win!<br />returning to lobby now...</span>
                 </>)
         }
         // If client is a hider and he lost
         else if (this.state.playerRole === "hider" && this.state.winner === "seeker") {
             comp =
                 (<>
-                    <h1>All of you were caught</h1>
-                    <h5>returning to lobby now...</h5>
+                    <span className="btn-loser ff-20" >All of you were caught<br />returning to lobby now...</span>
                 </>)
         } else {
             comp = (<></>)
