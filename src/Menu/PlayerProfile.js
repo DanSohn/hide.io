193cd1a723841a224883957e825f4f7fa4a51d70
Daniel <chiefgustavo000@gmail.com>
2020-04-15 22:53:04 -0600

    Placed profil items back into profile results

--- src/Menu/PlayerProfile.js
+++ src/Menu/PlayerProfile.js
@@ -10,7 +10,6 @@ import ClickSound from "../sounds/click";
 import { socket } from "../assets/socket";
 import { auth } from "../assets/auth";
 import { googleAuth } from "../Login/LoginScreen";
-import ProfileLabels from "./ProfileLabels";
 import ProfileResults from "./ProfileResults";
 
 const cookies = new Cookies();
@@ -26,9 +25,6 @@ class PlayerProfile extends Component {
             email: cookies.get("email"),
             signedIn: true,
             previous: false,
-            tWins: 0,
-            tGames: 0,
-            tLosses: 0,
         };
         this.goPrevious = this.goPrevious.bind(this);
     }
@@ -48,19 +44,7 @@ class PlayerProfile extends Component {
                 this.props.history.push('/');
             });
         });
-        // wait for player's stats
-        socket.on("player stats res", (player) => {
-            console.log("Received players information in Profile");
-            // if player exists and at least one of the two info (wins or games) are different, i update state
-            if (player) {
-                this.setState({
-                    tWins: player.totalWins,
-                    tGames: player.totalGamesPlayed,
-                    tLosses: player.totalGamesPlayed - player.totalWins,
-                })
 
-            }
-        });
     }
 
     componentWillUnmount() {
@@ -75,7 +59,6 @@ class PlayerProfile extends Component {
     }
 
     render() {
-        let ratio = this.state.tWins.toString() + " / " + this.state.tLosses.toString();
         let comp;
         if (!this.state.previous) {
             comp = (
@@ -86,53 +69,7 @@ class PlayerProfile extends Component {
                     />
                     <Break />
                     <div className="ContentScreen">
-                        <div className="profileData">
-                            <div className="profilePair">
-                                <span className="ff-20">Name:</span>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    id="userName"
-                                    placeholder={this.state.userName}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="profilePair">
-                                <span className="ff-20">Email:</span>
-                                <input
-                                    type="text"
-                                    id="email"
-                                    className="form-control"
-                                    placeholder={this.state.email}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="profilePair">
-                                <span className="ff-20">Win/Loss:</span>
-                                <input
-                                    type="text"
-                                    id="kdr"
-                                    className="form-control"
-                                    placeholder={ratio}
-                                    readOnly
-                                />
-                            </div>
-                            <div className="profilePair">
-                                <span className="ff-20">Total Games:</span>
-                                <input
-                                    type="text"
-                                    id="totalgames"
-                                    className="form-control"
-                                    placeholder={this.state.tGames}
-                                    readOnly
-                                />
-                            </div>
-                            {/* <ProfileLabels />
-                            <ProfileResults
-                                email={this.state.email}
-                                name={this.state.userName}
-                            /> */}
-                        </div>
+                        <ProfileResults name={this.state.userName} email={this.state.email} />
 
                     </div>
                 </div>
