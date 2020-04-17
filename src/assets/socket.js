a1e9ab2835badb01e8d7ac843d4d6f0475cad8a5
Trilok Patel <33501271+patelTrilok@users.noreply.github.com>
2020-04-16 18:44:39 -0600

    Update socket.js

--- src/assets/socket.js
+++ src/assets/socket.js
@@ -1,4 +1,7 @@
 import io from 'socket.io-client';
 
 // export const socket = io.connect('https://hideio.herokuapp.com');
-export const socket = io();
+export const socket = io.connect(process.env.PORT||'localhost:3001', {
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+});
