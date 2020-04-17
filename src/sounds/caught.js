23d3b3298e1d2c30e08d40ee1bb88ebc464a522b
PARSA.HONARMAND <parsa.honarmand@gmail.com>
2020-04-16 22:58:23 -0600

    changed volume

--- src/sounds/caught.js
+++ src/sounds/caught.js
@@ -1,6 +1,7 @@
 
 function caughtSound() {
     let soundButton = new Audio("https://www.myinstants.com/media/sounds/tindeck_1.mp3")
+    soundButton.volume = 0.2;
     return (
         soundButton.play()
     )
