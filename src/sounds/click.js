8a0ebb6d643945626a4247a745f4978cdb0a60ab
PARSA.HONARMAND <parsa.honarmand@gmail.com>
2020-04-16 23:15:12 -0600

    lowered volume

--- src/sounds/click.js
+++ src/sounds/click.js
@@ -1,5 +1,6 @@
 function click() {
     let soundButton = new Audio("https://vgmdownloads.com/soundtracks/nintendo-switch-sound-effects/xldszolu/Nock.mp3")
+    soundButton.volume = 0.3;
     return (
         soundButton.play()
     )
--- src/sounds/timer.js
+++ src/sounds/timer.js
@@ -1,6 +1,7 @@
 
 function timerSound() {
     let soundButton = new Audio("https://freesound.org/people/fordps3/sounds/186669/download/186669__fordps3__computer-boop.wav")
+    soundButton.volume = 0.3;
     return (
         soundButton.play()
     )
