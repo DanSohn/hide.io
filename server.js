a168d961d0f8e421802621e079b7af9869e0b8d9
Trilok Patel <33501271+patelTrilok@users.noreply.github.com>
2020-04-19 17:00:28 -0600

    Merged Master and prepared for heroku deployment

--- server.js
+++ server.js
@@ -7,17 +7,22 @@
  *
  */
 
-
+const path = require("path");
 const express = require("express");
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
+
+
 app.use(cors());
 app.get("/", (req, res) => {
     res.send("API working properly!");
 });
+
+//Provide a static version of the react app to serve to clients
+app.use(express.static(path.join(__dirname, './build')));
 
 // Database link
 const dbUtil = require("./dbUtils");
