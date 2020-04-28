0f11d87e04fd38ba5eced82a8b6b73706579ba9b
Daniel Sohn <chiefgustavo000@gmail.com>
2020-04-27 23:20:06 -0600

    Added frame for leaderboard

--- src/Leaderboard/Leaderboard.jsx
+++ src/Leaderboard/Leaderboard.jsx
@@ -1,7 +1,75 @@
 import React, { Component } from 'react';
+import {socket} from "../assets/socket";
+import {auth} from "../assets/auth";
+import {removeCookies} from "../assets/utils";
+import {googleAuth} from "../Login/LoginScreen";
+import ClickSound from "../sounds/click";
+import Header from "../assets/Header";
+import Break from "../assets/Break";
+import {Redirect} from "react-router-dom";
 
 class Leaderboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            previous: false
+        }
+        this.goPrevious = this.goPrevious.bind(this);
 
+    }
+
+    componentDidMount() {
+        socket.on("reconnect_error", (error) => {
+            // console.log("Error! Disconnected from server", error);
+            console.log("Error! Can't connect to server");
+            auth.logout(() => {
+                // reason history is avail on props is b/c we loaded it via a route, which passes
+                // in a prop called history always
+                removeCookies();
+                googleAuth.signOut();
+                console.log("going to logout!");
+                this.props.history.push('/');
+            });
+        });
+
+    }
+
+    componentWillUnmount() {
+        socket.off("reconnect_error");
+    }
+
+    goPrevious() {
+        ClickSound();
+        this.setState({
+            previous: true
+        });
+    }
+
+    render() {
+        let comp;
+        if (!this.state.previous) {
+            comp = (
+                <div className="GameWindow">
+                    <Header
+                        previous={this.goPrevious}
+                        title="Profile"
+                    />
+                    <Break />
+                    <div className="ContentScreen">
+                        Hello
+
+                    </div>
+                </div>
+            );
+        } else {
+            comp = (
+                <Redirect to="/MainMenu" />
+            );
+
+        }
+
+        return <>{comp}</>;
+    }
 }
 
 
